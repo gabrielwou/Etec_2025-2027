3d014a4bd0c7aad39fa2bb0812205ad0a4d67d32
gabrielwou <gabrielpenha302@gmail.com>
2026-02-11 15:28:09 -0300

    Delete 2°-ano/pw2/pastaTop directory

--- 2°-ano/pw2/pastaTop/EstruturaCondicional_If_else.js
+++
@@ -1,7 +0,0 @@
-/* O pesqueiro PESQPAG funciona da seguinte forma. Ao pescar um peixe, o preço é calculado de acordo com o peso do peixe, a saber:
-Se o peixe pesar até 3kg o quilograma do peixe custará R$ 12,00
-Se o peixe pesar mais de 3kg até 5kg o quilograma do peixe custará R$ 14,00
-Se o peixe pesar mais de 5kg até 10kg o quilograma do peixe custará R$ 15,00
-Se o peixe pesar mais de 10kg o quilograma do peixe custará R$ 17,00 e será ainda cobrada uma taxa adicional de R$ 8,00 para poder levar o peixe
-Criar um programa que leia o peso do peixe pescado em quilogramas e informe o valor a ser pago pelo pescador. */
-
--- 2°-ano/pw2/pastaTop/index.js
+++
@@ -1,3 +0,0 @@
-console.log("hello world")
-
-//node-- verso
