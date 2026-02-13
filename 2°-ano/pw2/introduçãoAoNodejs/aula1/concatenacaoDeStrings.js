8d305c1773ee7672cc7ce1b267db29bfbbb4c355
gabrielwou <gabrielpenha302@gmail.com>
2026-02-13 14:16:27 -0300

    Add files via upload

---
+++ 2°-ano/pw2/introduçãoAoNodejs/aula1/concatenacaoDeStrings.js
@@ -0,0 +1,9 @@
+let nome = "Gabriel";
+let sobrenome = "Penha";
+let nome_completo = "Seu nome completo é: "+nome+" "+sobrenome;
+console.log(nome_completo);
+
+let nome2 = "Wesley";
+let sobrenome2 = "Fernandes";
+let nome_completo2 = "Seu nome completo é: "+nome2+" "+sobrenome2;
+console.log(nome_completo2);
---
+++ 2°-ano/pw2/introduçãoAoNodejs/aula1/estruturaCondicionalCase.js
@@ -0,0 +1,36 @@
+const diaSemana = 3;
+let nomeDia;
+switch (diaSemana){
+    case 0:
+        nomeDia = "Segunda-feira"
+        break;
+    case 1:
+        nomeDia = "Terça-feira"
+        break;
+    case 2:
+        nomeDia = "Quarta-feira"
+        break;
+    case 3:
+        nomeDia = "Quinta-feira"
+        break;
+    case 4:
+        nomeDia = "Sexta-feira"
+        break;
+}
+console.log("Hoje é : "+nomeDia);
+
+
+const carro = 1;
+let marcaCarro;
+switch (carro){
+    case 0:
+        marcaCarro = "Chevrolet"
+        break;
+    case 1:
+        marcaCarro = "Volkswagen"
+        break;
+    case 2:
+        marcaCarro = "Fiat"
+        break;
+}
+console.log("A marca do seu carro é : "+marcaCarro)
