a2b391dd09c71c7d490cdddff3c8c3e4e8a9ff9a
gabrielwou <gabrielpenha302@gmail.com>
2026-02-23 08:40:15 -0300

    Add files via upload

---
+++ 2°-ano/pam1/exercicio_js1/js/script.js
@@ -0,0 +1,8 @@
+
+
+let nota_1 = document.querySelector('.nota1');
+let nota_2 = document.querySelector('.nota2');
+let nota_3 = document.querySelector('.nota3');
+
+let media = (nota_1+nota_2+nota_3)/3;
+console.log(media)
