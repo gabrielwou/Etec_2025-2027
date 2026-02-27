6f1f292fe1fdf29f82bca3498801c72ef00b9516
gabrielwou <gabrielpenha302@gmail.com>
2026-02-27 08:27:39 -0300

    Add files via upload

---
+++ 2°-ano/ds1/dsAula1/Exercicio3.java
@@ -0,0 +1,22 @@
+package dsAula1;
+
+public class Exercicio3 {
+	public static void main(String[] args) {
+		int [][] m = new int[4][4];
+		int a = 1;
+		for(int i=0;i<4;i++) {
+			for(int j=0;j<4;j++) {
+				m[i][j]=a;
+				a++;
+			}
+
+		}
+		
+		for(int i=0;i<4;i++) {
+				System.out.println(m[i][i]);
+		}
+		
+		
+		
+	}
+}
