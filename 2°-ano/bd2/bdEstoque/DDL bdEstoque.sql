f012a751d3fa40ffb4fff35cfe8edc826822cae5
gabrielwou <gabrielpenha302@gmail.com>
2026-03-14 16:55:05 -0300

    Update DDL bdEstoque.sql

--- 2°-ano/bd2/bdEstoque/DDL bdEstoque.sql
+++ 2°-ano/bd2/bdEstoque/DDL bdEstoque.sql
@@ -45,12 +45,3 @@ CREATE TABLE tbItensVenda(
 	quantidadeItensVenda INT,
 	subTotalItensVenda	 INT
 );
-
-INSERT INTO tbCliente (nomeCliente,cpfCliente,emailCliente,sexoCliente,dataNascimentoCliente)
-VALUES
-('Armando José Santana',12345678900,'armandojsantana@outlook.com','m','1961-02-21 00:00:00'),
-('Sheila Carvalho Leal',45678909823,'scarvalho@g.com.br','f','1978-09-13 00:00:00'),
-('Carlos Henrique Souza',76598278299,'cherique@g.com.br','m','1981-09-08 00:00:00'),
-('Maria Aparecida Souza',87365309899,'mapdasouza@outlook.com','f','1962-07-07 00:00:00'),
-('Adriana Nogueira Silva',76354309388,'drica1977@g.com.br','f','1977-04-09 00:00:00'),
-('Paulo Henrique Silva',87390123111,'phsilva80@hotmail.com','m','1987-02-02 00:00:00');
